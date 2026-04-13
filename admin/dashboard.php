087e98d9ad4581054c43bb7165119ac2327b5c4a
dzakimutammadien <dzakimutamm@gmail.com>
2026-04-14 02:43:33 +0700

    verifikasi donasi

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -413,10 +413,7 @@ $recentDonasi = query($queryRecent);
                 <i class="fas fa-chevron-down arrow"></i>
             </div>
             <div class="submenu">
-                <div class="submenu-item" onclick="location.href='verifikasi_donasi.php'">
-                    <i class="fas fa-hand-holding-heart"></i>
-                    <span>Donasi Donatur</span>
-                </div>
+                <div class="submenu-item active" onclick="location.href='verifikasi_donasi.php'"><i class="fas fa-hand-holding-heart"></i><span>Donasi Donatur</span></div>
                 <div class="submenu-item" onclick="location.href='verifikasi_pengeluaran.php'">
                     <i class="fas fa-money-bill-wave"></i>
                     <span>Pengeluaran Panti</span>
